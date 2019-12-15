160a98e65bd41289e6839e83fefc36255fd27f3f
jmbyam <jmbyam@alaska.edu>
2019-12-15 12:25:42 -0900

    Update prims.hpp

--- prims.hpp
+++ prims.hpp
@@ -14,36 +14,37 @@ using std::cout;
 using std::endl;
 #include <vector>
 using std::vector;
-#include <queue>
-using std::priority_queue;
-
-struct my_comparator
-{
-    bool operator()(std::vector<int> const& a, std::vector<int> const& b) const
-    {
-        return a[2] > b[2];
-    }
-};
-
-
-using my_priority_queue = priority_queue<std::vector<int>, std::vector<std::vector<int>>, my_comparator>;
+#include <algorithm>
+using std::any_of;
 
 void prims( vector<vector<int>> graph ) {
     
     cout << "The minimum spanning tree of the given graph is..." << endl;
     
-    my_priority_queue min;
-    for ( auto edge : graph ) {
-        min.push(edge);
-    }
+    std::sort(graph.begin(), graph.end(),
+              [](const std::vector<int>& a, const std::vector<int>& b) {
+      return a[2] > b[2];
+    });
     
-    while( min.size() != 0 ) {
-        cout << "{" << min.top().at(0) << ", " << min.top().at(1) << ", " << "}" << endl;
-        int kill = min.top().at(0);
-        // go through min and remove all the edges that have the same starting value as the one you display
-        for ( auto edge : min ) {
-            if ( edge[0] == kill )
-                // remove edge some how
+    while( graph.size() != 0 ) {
+        cout << "{" << graph[graph.size()-1][0] << ", " << graph[graph.size()-1][1] << ", " << graph[graph.size()-1][2] << "}" << endl;
+        
+        // add first and second element to list of vertexes if they aren't already there
+        vector<int> reached ={};
+        if (std::find(reached.begin(), reached.end(), graph[graph.size()-1][0]) == reached.end()) {
+          reached.push_back(graph[graph.size()-1][0]);
+        }
+        if (std::find(reached.begin(), reached.end(), graph[graph.size()-1][1]) == reached.end()) {
+          reached.push_back(graph[graph.size()-1][1]);
+        }
+        // if a vector has a first element and second element that have both been reached, remove it.
+        int counter = 0;
+        for ( vector<int> edge : graph ) {
+            //if ( edge.size() == 0 ) continue;
+            if ( any_of(reached.begin(), reached.end(), [edge](int i){return i==edge[0];}) && any_of(reached.begin(), reached.end(), [edge](int i){return i==edge[1];}) ){
+                graph.erase(graph.begin()+counter);
+            }
+            counter++;
         }
     }
     
